6f760482377e3795449ab62b6f277095b6de654a
Derek Peacock <derek.peacock@yahoo.com>
2023-01-18 11:38:57 +0000

    Removed unused usings

--- CarsMVC/Controllers/CarsController.cs
+++ CarsMVC/Controllers/CarsController.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 using CarsMVC.Data;
 using CarsMVC.Models;
-using Microsoft.AspNetCore.Authorization;
 
 namespace CarsMVC.Controllers
 {
