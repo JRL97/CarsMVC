8724e06b77fd0c2a25db44834a216a48abd60806
Derek Peacock <derek.peacock@yahoo.com>
2023-01-18 11:43:37 +0000

    Started RolesController

---
+++ CarsMVC/Controllers/RolesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace CarsMVC.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager= roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var roles = _roleManager.Roles.ToList();
+            return View(roles);
+        }
+    }
+}
